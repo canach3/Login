8bf31d9454d02fe21a6b34cdbaa0e42ac6659286
JungYoon <canach3@gmail.com>
2025-07-19 17:44:08 +0900

    feat: 도메인별 ErrorCode 및 공통 ResponseCode, SuccessCode 정의

---
+++ src/main/java/boardProject/board/common/code/ResponseCode.java
@@ -0,0 +1,11 @@
+package boardProject.board.common.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface ResponseCode {
+    HttpStatus getHttpStatus();
+    String getMessage();
+    default int getStatusValue() {
+        return getHttpStatus().value();
+    }
+}
