d531009901bd78b51ab374a1707db7cd3e941120
JungYoon <canach3@gmail.com>
2025-07-20 20:51:26 +0900

    feat: 예외처리 공통 클래스 추가

--- src/main/java/boardProject/board/auth/exception/InvalidLoginInfoException.java
+++ src/main/java/boardProject/board/auth/exception/InvalidLoginInfoException.java
@@ -1,7 +1,6 @@
 package boardProject.board.auth.exception;
 
 import boardProject.board.common.code.AuthErrorCode;
-import boardProject.board.common.code.ResponseCode;
 
 public class InvalidLoginInfoException extends AuthException {
 
