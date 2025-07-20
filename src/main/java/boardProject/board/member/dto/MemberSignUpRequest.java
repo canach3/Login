5a92514dad1ad7ee807d5bcfbf45ac180f5016ac
JungYoon <canach3@gmail.com>
2025-07-20 20:42:49 +0900

    rename: Member 관련 DTO 이름 변경

--- src/main/java/boardProject/board/member/dto/MemberSignUpRequest.java
+++ src/main/java/boardProject/board/member/dto/MemberSignUpRequest.java
@@ -3,10 +3,8 @@ package boardProject.board.member.dto;
 import lombok.Data;
 
 @Data
-public class MemberSaveRequest {
+public class MemberSignUpRequest {
     private String loginId;
     private String password;
     private String name;
-
-
 }
