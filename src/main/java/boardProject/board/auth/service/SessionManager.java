be097bcf6c6498b0740625a6fda400726a4f57b1
JungYoon <canach3@gmail.com>
2025-07-31 18:22:44 +0900

    fix: 사용하지 않는 라이브러리 제거

--- src/main/java/boardProject/board/auth/service/SessionManager.java
+++ src/main/java/boardProject/board/auth/service/SessionManager.java
@@ -6,7 +6,6 @@ import boardProject.board.member.entity.Member;
 import boardProject.board.member.repository.MemberRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
