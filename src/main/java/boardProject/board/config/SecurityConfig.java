81ff700a055c910b9946e0ac2037e2112b229d0b
JungYoon <canach3@gmail.com>
2025-08-05 21:40:29 +0900

    feat: SecurityConfig 코드 추가(Spring Security 적용)

--- src/main/java/boardProject/board/config/SecurityConfig.java
+++ src/main/java/boardProject/board/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package boardProject.board.config;
 
+import boardProject.board.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -21,10 +27,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화 (POST 요청 테스트를 위해)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인폼 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용
+                                .anyRequest().permitAll()
+
+                        // TODO: 아래는 시큐리티 기반 로그인 코드 적용시 활성화 할 것
+//                        .requestMatchers("/api/members/signup", "/api/auth/login").permitAll() // 로그인을 하지 않아도 되는 경로
+//                        .anyRequest().authenticated() // 나머지 경로는 로그인 성공시 허용
                 )
+                .userDetailsService(customUserDetailsService)
                 .build();
     }
 }
