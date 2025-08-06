d9cd8c87e5d3c60e3833dc0dfc568df5d1459a7f
김정윤 <canach3@gmail.com>
2025-08-06 19:13:31 +0900

    Update AuthErrorCode.java

--- src/main/java/boardProject/board/common/code/AuthErrorCode.java
+++ src/main/java/boardProject/board/common/code/AuthErrorCode.java
@@ -12,7 +12,7 @@ public enum AuthErrorCode implements ResponseCode{
     ACCESS_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증되지 않은 접근입니다."),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다.");
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "로그인 정보가 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
