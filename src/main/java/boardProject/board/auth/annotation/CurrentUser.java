3d575510607ed641d2e83c1677ea649fa6df8931
JungYoon <canach3@gmail.com>
2025-08-07 20:49:51 +0900

    remove: Spring Security 적용시 필요 없는 클래스 제거

---
+++ src/main/java/boardProject/board/auth/annotation/CurrentUser.java
@@ -0,0 +1,9 @@
+//package boardProject.board.auth.annotation;
+//
+//import java.lang.annotation.*;
+//
+//@Target(ElementType.PARAMETER)
+//@Retention(RetentionPolicy.RUNTIME)
+//@Documented
+//public @interface CurrentUser {
+//}
