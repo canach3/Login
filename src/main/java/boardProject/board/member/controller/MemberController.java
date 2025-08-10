20de2371516a782801ab760e6e93c4623af306bf
JungYoon <canach3@gmail.com>
2025-08-10 21:01:52 +0900

    fix: 불필요한 주석 제거

--- src/main/java/boardProject/board/member/controller/MemberController.java
+++ src/main/java/boardProject/board/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package boardProject.board.member.controller;
 
-import boardProject.board.auth.annotation.CurrentUser;
-import boardProject.board.auth.model.LoginMember;
+import boardProject.board.auth.model.CustomUserDetails;
 import boardProject.board.common.response.ApiResponse;
 import boardProject.board.member.dto.MemberInfoResponse;
 import boardProject.board.member.dto.MemberSignUpRequest;
 import boardProject.board.member.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static boardProject.board.common.code.SuccessCode.*;
@@ -25,19 +24,9 @@ public class MemberController {
         return ResponseEntity.ok(ApiResponse.success(NO_CONTENT));
     }
 
-//    // setAttribute, getAttribute 방법
-//    @GetMapping("/me")
-//    public ResponseEntity<ApiResponse<MemberInfoResponse>> getMyInfo(HttpServletRequest request) {
-//        LoginMember loginMember = (LoginMember) request.getAttribute("loginMember");
-//        MemberInfoResponse memberInfoResponse = memberService.getMyInfo(loginMember.id());
-//
-//        return ResponseEntity.ok(ApiResponse.success(OK, memberInfoResponse));
-//    }
-
-    // @CurrentUser 커스텀 애노테이션 방법
     @GetMapping("/me")
-    public ResponseEntity<ApiResponse<MemberInfoResponse>> getMyInfo(@CurrentUser LoginMember loginMember) {
-        MemberInfoResponse memberInfoResponse = memberService.getMyInfo(loginMember.id());
+    public ResponseEntity<ApiResponse<MemberInfoResponse>> getMyInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        MemberInfoResponse memberInfoResponse = memberService.getMyInfo(userDetails.getId());
 
         return ResponseEntity.ok(ApiResponse.success(OK,memberInfoResponse));
     }
