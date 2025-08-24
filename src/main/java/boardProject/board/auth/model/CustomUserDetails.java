3562fe2d9a1980a8d4e1f9b05c77a6a7212b57d2
JungYoon <canach3@gmail.com>
2025-08-24 14:51:36 +0900

    feat: SecurityConfig 디렉토리 변경, CustomUserDetails에 memberId 추가

--- src/main/java/boardProject/board/auth/model/CustomUserDetails.java
+++ src/main/java/boardProject/board/auth/model/CustomUserDetails.java
@@ -28,4 +28,6 @@ public class CustomUserDetails implements UserDetails {
     public String getUsername() {
         return member.getLoginId();
     }
+
+    public long getId() {return member.getId();}
 }
