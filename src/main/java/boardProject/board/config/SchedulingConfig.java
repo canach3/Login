cb55f34f565d5ae7a7bbba4c35685da232e76791
JungYoon <canach3@gmail.com>
2025-08-27 15:47:29 +0900

    feat: 세션 유효시간, 스케쥴러 작동 주기 직접 설정

--- src/main/java/boardProject/board/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package boardProject.board.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling // 스케줄링 활성화
-public class SchedulingConfig {
-}   
