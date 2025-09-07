6bd61722d0a27d064c13601936452dfc64766c43
JungYoon <canach3@gmail.com>
2025-09-07 15:36:06 +0900

    ResponseEntity -> ApiResponse 방식으로 변경

--- src/main/java/LoginProject/login/member/controller/MemberController.java
+++ src/main/java/LoginProject/login/member/controller/MemberController.java
@@ -4,8 +4,10 @@ import LoginProject.login.common.response.ApiResponse;
 import LoginProject.login.member.dto.MemberSignUpRequest;
 import LoginProject.login.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static LoginProject.login.common.code.SuccessCode.NO_CONTENT;
 
@@ -16,8 +18,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<Void>> signUp(@RequestBody MemberSignUpRequest memberSignUpRequest) {
+    public ApiResponse<Void> signUp(@RequestBody MemberSignUpRequest memberSignUpRequest) {
         memberService.save(memberSignUpRequest);
-        return ResponseEntity.ok(ApiResponse.success(NO_CONTENT));
+        return ApiResponse.success(NO_CONTENT);
     }
 }
