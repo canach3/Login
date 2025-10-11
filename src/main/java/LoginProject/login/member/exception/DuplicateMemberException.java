072d7cad235f9ef401959ae729247803dfb61a1e
JungYoon <canach3@gmail.com>
2025-10-11 17:38:21 +0900

    refactor: ApiResponse, 커스텀 예외 클래스 사용X

--- src/main/java/LoginProject/login/member/exception/DuplicateMemberException.java
+++
@@ -1,9 +0,0 @@
-package LoginProject.login.member.exception;
-
-import LoginProject.login.common.code.MemberErrorCode;
-
-public class DuplicateMemberException extends MemberException{
-    public DuplicateMemberException() {
-        super(MemberErrorCode.DUPLICATE_MEMBER);
-    }
-}
--- src/main/java/LoginProject/login/member/exception/MemberNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package LoginProject.login.member.exception;
-
-import LoginProject.login.common.code.MemberErrorCode;
-
-public class MemberNotFoundException extends MemberException{
-    public MemberNotFoundException() {
-        super(MemberErrorCode.MEMBER_NOT_FOUND);
-    }
-}
